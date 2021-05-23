6ed3b26f46889a91685d0f75e26555d8bf160179
Linshengyi-William <sunlins@oregonstate.edu>
2021-05-22 18:27:46 -0700

    leapYear code done

--- unittest/leapYear.py
+++ unittest/leapYear.py
@@ -1,15 +1,20 @@
 #partial code adapted from:https://www.javatpoint.com/python-check-leap-year
-year = int(input("Enter a year: "))  
-if (999 < year <= 10000):
-    if (year % 4) == 0:  
-        if (year % 100) == 0:  
-            if (year % 400) == 0:  
-                print(year,"is a leap year")  
+def isLeapYear(year)
+    if (999 < year <= 10000):
+        if (year % 4) == 0:  
+            if (year % 100) == 0:  
+                if (year % 400) == 0:  
+                    return True
+                    #print(year,"is a leap year")  
+                else:  
+                    return False
+                    #print(year,"is not a leap year")  
             else:  
-                print(year,"is not a leap year")  
-        else:  
-            print(year,"is a leap year")  
-    else:  
-        print(year,"is not a leap year") 
-else:
-    print("Invlid year, entered year should be a four digit number.")
+                return True
+                #print(year,"is a leap year")  
+        else: 
+            return False 
+            #print(year,"is not a leap year") 
+    else:
+        return False
+        #print("Invlid year, entered year should be a four digit number.")
